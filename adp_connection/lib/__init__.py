464982ae51e34d27582064c740474bc5382840eb
Hailey Worthy <hailey@haileyworthy.com>
2020-04-17 10:11:13 -0400

    update relative paths

--- adp_connection/lib/__init__.py
+++ adp_connection/lib/__init__.py
@@ -18,7 +18,7 @@
 # express or implied.  See the License for the specific language
 # governing permissions and limitations under the License.
 
-from connectionconfiguration import *
-from adpapiconnection import *
-from connectexceptions import *
-from adpapiconnectionfactory import *
+from .connectionconfiguration import *
+from .adpapiconnection import *
+from .connectexceptions import *
+from .adpapiconnectionfactory import *
